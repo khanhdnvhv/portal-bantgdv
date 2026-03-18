f3b4b9595f44320b6ed6a8226c7715f427420f67
khanhdn <khanhdn1@vhv.vn>
2026-03-18 20:30:30 +0700

    end

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,12 @@ import tailwindcss from "@tailwindcss/vite";
 import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
+  // THÊM DÒNG NÀY: Thay 'portal-bantgdv' bằng tên chính xác repository của bạn
+  base: "/portal-bantgdv/", 
+  
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      // Dòng này giúp Vite hiểu figma:asset/ chính là thư mục src/assets/ của bạn
       "figma:asset": fileURLToPath(new URL("./src/assets", import.meta.url)),
     },
   },
