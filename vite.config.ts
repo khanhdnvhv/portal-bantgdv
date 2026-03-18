5e523f8029d0c2fb5d78c548078c62393300eed5
khanhdn <khanhdn1@vhv.vn>
2026-03-18 20:38:31 +0700

    ss

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
-  // THÊM DÒNG NÀY (Nhớ có dấu gạch chéo ở cả 2 đầu)
+  // THÊM DÒNG NÀY (Đúng tên repository của bạn trên GitHub)
   base: "/portal-bantgdv/", 
 
   plugins: [react(), tailwindcss()],
