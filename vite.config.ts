e867aea15449f9e5a1595ae7e84616632d5aca00
khanhdn <khanhdn1@vhv.vn>
2026-03-18 20:26:29 +0700

    ok3

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      // Dòng này giúp Vite hiểu figma:asset/ chính là thư mục src/assets/ của bạn
+      "figma:asset": fileURLToPath(new URL("./src/assets", import.meta.url)),
+    },
+  },
 });
